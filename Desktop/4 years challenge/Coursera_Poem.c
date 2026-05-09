8194234e5e1d9ac477b5ab701c81e1f7fe6300d2
podarays <kwesiedwin482@gmail.com>
2026-05-09 07:14:51 -0400

    Added my poem

---
+++ Desktop/4 years challenge/Coursera_Poem.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void)
+{
+    printf("Today is a peaceful day.\n\n");
+    printf("It is a day The Lord has made\n");
+    printf("We will be glad and rejoice in it");
+    return 0;
+}
