8416c91dfb46a3b6b0ac15c5255323b1f5fff411
podarays <kwesiedwin482@gmail.com>
2026-05-10 11:04:16 -0400

    Tried something fun

---
+++ Desktop/4YearsChallenge/funexperiment.c
@@ -0,0 +1,10 @@
+/* Fun Experiment
+   Edwin
+   May 10, 2026
+*/
+#include<stdio.h>
+int main(void)
+{
+    char myInitial = 'E';
+    printf("My initial is %c and its secret number is %d\n", myInitial, myInitial);
+}
